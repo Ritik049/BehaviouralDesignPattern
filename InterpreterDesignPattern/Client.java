ab14faae3f16d30412449ab57f7575a15acef616
Ritik049 <141677435+Ritik049@users.noreply.github.com>
2024-04-19 15:48:47 +0000

    Interface Design Pattern

--- InterpreterDesignPattern/Client.java
+++ InterpreterDesignPattern/Client.java
@@ -1,5 +1,5 @@
 public class Client
-{
+{ //Here we make  interpret function in  expression itself to avoid using interface and expression class again and again,
     public static void main(String[]args)
     {
         AddExpression expression = new AddExpression(new NumberExpression(10),new SubtractExpression(new NumberExpression(20),new NumberExpression(5)));
